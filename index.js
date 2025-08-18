adf17df27ee0ee935c1554f7a78221fa94750a5a
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-18 11:07:27 +0600

    post api create

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect()
+
+    const database = client.db('coffee-shop')
+    const coffeeCollection = database.collection('all-coffee')
+
+    // post route
+    app.post('/coffee', async (req, res) => {
+      const result = req.body
+      console.log(result)
+      const postData = await coffeeCollection.insertOne(result)
+      res.send(postData)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
@@ -31,7 +43,7 @@ async function run() {
     )
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close()
+    // await client.close()
   }
 }
 run().catch(console.dir)
